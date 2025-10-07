14e04955da2fb1c2eb5504e01f78e78ea3fce91c
virani dhvani <dhvanivirani2109@gmail.com>
2025-10-07 13:33:46 +0530

    all

---
+++ ass_1062_1.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main() {
+    int n, i, smallest;
+    
+    printf("Enter the number of elements: ");
+    scanf("%d", &n);
+
+    int arr[n];   // Declare array after reading n (C99 feature)
+
+    printf("Enter %d elements:\n", n);
+    for (i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    smallest = arr[0];
+    for (i = 1; i < n; i++) {
+        if (arr[i] < smallest) {
+            smallest = arr[i];
+        }
+    }
+
+    printf("The smallest element is: %d\n", smallest);
+return 0;
+}
